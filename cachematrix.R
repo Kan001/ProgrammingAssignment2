8ec29d547b43e0e64f9b6ec56cb63c4b28c685b3
Kan001 <gliderpilot.jim@gmail.com>
2014-05-24 22:45:04 -0500

    Commit 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,21 +1,25 @@
-## The function makeCacheMatrix takes a matix as input (or default) and calls 
-## the function cacheSolve to compute the inverse of that matrix.
-## To save computing cycles cacheSolve will cache matrices identical to the preceding call
+## The function makeCacheMatrix takes a martix as input and calls the function
+## cacheSolve to compute the inverse of that matrix.
 ##
 makeCacheMatrix <- function(x=matrix()) {
+## Function receives a matrix, if empty then an empty matrix is returned.
+## If a valid matix is received the function cacheSolve is called to compute the inverse matrix.
+##
   if (is.na(x[1,1])) {
     cat("Empty matix received","\n")
     return(x)
   }
   inv_mat <- cacheSolve(x)
-  o_matc <<- inv_mat  
   cat("The inverse matrix is :","\n")
-  print(o_matc)
+  print(inv_mat)
   i_matc <<- x
 }
 ## 
 cacheSolve <- function(imat) {
-## Return a matrix that is the inverse of 'x'
+## Compute the inverse of the matrix passed to this function (solve function). If the
+## incoming matrix is the same as in the previous call, retrieve the inverse from cache.
+## If this is a new matrix the inverse will be stored in cache.
+##
   if (identical(imat,i_matc) ) {
     cat("Retrieving inverse matrix from cache...\n")
     return(o_matc)
